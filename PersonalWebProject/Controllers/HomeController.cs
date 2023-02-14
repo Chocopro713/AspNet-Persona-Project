8a578fcf4aa18f1e88b44cb138ada3fab5df47c6
Cristian Barbosa <Cristianbr7@live.com>
2023-02-14 15:36:07 -0500

    first commit

---
+++ PersonalWebProject/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWebProject.Models;
+using System.Diagnostics;
+
+namespace PersonalWebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var persona = new Persona()
+            {
+                Nombre = "Choco Choco",
+                Edad = 21
+            };
+            return View(persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PersonalWebProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalWebProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
